12a2b3a6ff6bb5c5d3e9516b84238a1062736819
SheifelJanna <80528701+SheifelJanna@users.noreply.github.com>
2022-04-14 00:55:48 +0500

    ЛР №2
    Задания 1-7 ЛР 2

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<p>Лабораторная работа № 1<p> 
-    <ul>
- <li><a href="Lab1.html">Задание 1</a></li>
+<body style='background-color:#FFF0F5'>
+<center><p>Лабораторная работа № 1<p></center>
+ <center><li><a href="Lab1.html">Задание 1</a></li>
  <li><a href="Lab2.html">Задание 2</a></li>
  <li><a href="Lab3.html">Задание 3</a></li>
  <li><a href="Lab4.html">Задание 4</a></li>
  <li><a href="Lab5.html">Задание 5</a></li>
  <li><a href="Lab6.html">Задание 6</a></li>
- <li><a href="Lab7.html">Задание 7</a></li>
+ <li><a href="Lab7.html">Задание 7</a></li></center>
+
+ <center><p>Лабораторная работа № 2<p></center>
+ <center><li><a href="lab_2-1.html">Задание 1</a></li>
+ <li><a href="lab_2-2.html">Задание 2</a></li>
+ <li><a href="lab_2-3.html">Задание 3</a></li>
+ <li><a href="lab_2-4.html">Задание 4</a></li>
+ <li><a href="lab_2-5.html">Задание 5</a></li>
+ <li><a href="lab_2-6.html">Задание 6</a></li>
+ <li><a href="lab_2-7.html">Задание 7</a></li></center>
